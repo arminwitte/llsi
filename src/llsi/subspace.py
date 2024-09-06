565b21e7bdc3ca6fc7b92cd79266e46c0cb3da3f
Armin Witte <witte.armin@gmail.com>
2024-09-06 13:37:45 +0200

    mimo seems to be working

--- src/llsi/subspace.py
+++ src/llsi/subspace.py
@@ -28,11 +28,6 @@ class SubspaceIdent(SysIdAlgBase):
                 x_ = x_.ravel().T
                 A.append(x_[i : -n + i])
 
-        # for x_ in x.T:
-        #     x_ = x_.ravel().T
-        #     for i in range(n):
-        #         A.append(x_[i : -n + i])
-
         return np.array(A)
 
     def _abcd_state(self, Xf, s, n, r):
@@ -70,63 +65,28 @@ class SubspaceIdent(SysIdAlgBase):
 
         # ====================================================================
 
-        print(f"U2.T shape: {U2.T.shape}")
         P = np.split(U2.T, U2.T.shape[1] // ny, axis=1)
-        print(f"Pi shape: {P[0].shape}")
 
         nn = len(P) * P[0].shape[0]
-        print(f"nn: {nn}")
         rny = r
-        print(f"rny: {rny}")
         r = r // ny
-        print(f"r: {r}")
-        print(f"n: {n}")
         A_ = np.zeros((nn, ny + n))
-        print(f"A_ shape: {A_.shape}")
         P1 = np.vstack(P)
         rr = P[0].shape[0]
-        # for i, Pi in enumerate(P):
-        #     A_[i * rr : (i + 1) * rr, :ny] = Pi
         A_[:, :ny] = P1
 
         for i in range(1, r):
-            print(i)
             Pi_tilda = np.hstack(P[i:])
-            print(f"Pi_tilda shape: {Pi_tilda.shape}")
             Ori = Or[: rny - (ny * i)]
-            print(f"Ori shape: {Ori.shape}")
             N = Pi_tilda @ Ori
-            print(f"N shape: {N.shape}")
             A_[(i - 1) * rr : i * rr, ny:] = N
 
-        # P = U2.T.reshape(-1,ny,order="F")
-        # # print(P)
-        # # A1_ = P.ravel(order="F")
-        # A1_ = P
-
-        # nn = A1_.shape[0]
-        # A_ = np.zeros((nn, ny + n))
-        # A_[:, 0:ny] = A1_.reshape(-1, ny)
-
-        # for i in range(1, r):
-        #     Pi = P[:, i:r]
-        #     print(f"Pi shape: {Pi.shape}")
-        #     Oi = Or[0 : r - i, :]
-        #     print(f"Oi shape: {Oi.shape}")
-        #     Ni = Pi @ Oi
-        #     j = (i - 1) * (r - n)
-        #     A_[j : j + Ni.shape[0], ny : ny + Ni.shape[1]] = Ni
-
-        # print(A_)
-
-        # M = (U2.T @ L31 @ np.linalg.inv(L11)).reshape(-1, ny, order="F")
         M = U2.T @ L31 @ np.linalg.inv(L11)
         Mi = np.split(M, M.shape[1] // nu, axis=1)
         M = np.vstack(Mi)
         print(f"M shape: {M.shape}")
 
         x_, *_ = scipy.linalg.lstsq(A_, M)
-        # x_ = scipy.linalg.pinv(A_) @ M
 
         D = x_[:ny, :]
         B = x_[ny:, :]
@@ -166,45 +126,34 @@ class N4SID(SubspaceIdent):
         Uf = U[r : 2 * r, :]
 
         Wp = np.vstack((Up, Yp))
-        # Wf = np.vstack((Uf,Yf))
         Psi = np.vstack((np.vstack((Uf, Wp)), Yf))
 
         L, Q = self.lq(Psi)
-
-        L11 = L[0:r, 0:r]
-        # L12 = L[  0:r  ,r:2*r]
-        # L21 = L[  r:3*r,0:r]
         L22 = L[r : 3 * r, r : 3 * r]
-        L31 = L[3 * r : 4 * r, 0:r]
         L32 = L[3 * r : 4 * r, r : 3 * r]
 
-        # print(L32.shape, np.linalg.pinv(L22).shape, Wp.shape)
         Gamma_r = L32 @ np.linalg.pinv(L22) @ Wp  # oblique projection
-        # print(Gamma_r.shape)
+
         U_, s_, V_ = scipy.linalg.svd(Gamma_r, full_matrices=False)
-        # print(U_.shape, s_.shape, V_.shape)
 
         self.singular_values = s_
+        Sigma_sqrt = np.diag(np.sqrt(s_[:n]))
 
+        # ====================================================================
+        # V1 = V_[0:n, :]
+        # Xf = (
+        #     Sigma_sqrt @ V1
+        # )  # state matrix # TANGIRALA SAYS IT SHOULD BE TRANSPOSED !?!
+
+        # A, B, C, D = self._abcd_state(Xf, s, n, r)
+        # ====================================================================
         U1 = U_[:, 0:n]
         U2 = U_[:, n:r]
-        Sigma_sqrt = np.diag(np.sqrt(s_[:n]))
-        # Sigma_sqrt = np.zeros(Gamma_r.shape, s_.dtype)
-        # np.fill_diagonal(Sigma_sqrt, np.sqrt(s_))
-        # Sigma_sqrt = Sigma_sqrt[:n, :]
-        # Sigma_sqrt = scipy.linalg.diagsvd(s_, *Gamma_r.shape)
-        V1 = V_[0:n, :]
-        # V1 = V_[:, :n]
-        # V2 = V_[:,n:r]
+        L11 = L[0:r, 0:r]
+        L31 = L[3 * r : 4 * r, 0:r]
 
-        # Or = U1 @ Sigma_sqrt  # extended observability matrix
-        Xf = (
-            Sigma_sqrt @ V1
-        )  # state matrix # TANGIRALA SAYS IT SHOULD BE TRANSPOSED !?!
-
-        # print(s,n,r)
-        A, B, C, D = self._abcd_state(Xf, s, n, r)
-        # A, B, C, D = self._abcd_observability_matrix(self, U1, U2, L11, L31, Sigma_sqrt, n, r):
+        A, B, C, D = self._abcd_observability_matrix(U1, U2, L11, L31, Sigma_sqrt, n, r)
+        # ====================================================================
 
         mod = StateSpaceModel(A=A, B=B, C=C, D=D, Ts=self.Ts)
         mod.info["Hankel singular values"] = s_
@@ -214,91 +163,6 @@ class N4SID(SubspaceIdent):
     @staticmethod
     def name():
         return "n4sid"
-
-
-# class N4SID2(SubspaceIdent):
-#     def __init__(self, data, y_name, u_name, settings={}):
-#         super().__init__(data, y_name, u_name, settings=settings)
-
-#         # estimate extended observability matrix and states. Then estimate A, B, C, and D in one go.
-#         # (Tangirala 2014)
-
-#     def ident(self, order):
-#         if isinstance(order, (tuple, list)):
-#             n = order[0]
-#         else:
-#             n = order
-
-#         r = 2 * n + 1  # window length
-
-#         Y = self.hankel(self.y, 2 * r)
-#         U = self.hankel(self.u, 2 * r)
-
-#         s = Y.shape[1]
-
-#         Yp = Y[0:r, :]
-#         YpPlus = Y[0:r+1, :]
-#         Up = U[0:r, :]
-#         UpPlus = U[0:r+1, :]
-
-#         Yf = Y[r : 2 * r, :]
-#         YfMinus = Y[r+1 : 2 * r, :]
-#         Uf = U[r : 2 * r, :]
-#         UfMinus = U[r+1 : 2 * r, :]
-
-#         Wp = np.vstack((Up, Yp))
-#         WpPlus = np.vstack((UpPlus, YpPlus))
-#         # Wf = np.vstack((Uf,Yf))
-
-
-#         # Projection 1
-#         Psi = np.vstack((np.vstack((Uf, Wp)), Yf))
-
-#         L, _ = self.lq(Psi)
-
-#         L22 = L[r : 3 * r, r : 3 * r]
-#         L32 = L[3 * r : 4 * r, r : 3 * r]
-
-
-#         # Projection 2
-#         PsiAst = np.vstack((np.vstack((Uf, Wp)), Yf))
-
-#         L, _ = self.lq(PsiAst)
-
-#         LAst22 = L[r-1 : 3 * r+1, r-1 : 3 * r+1]
-#         LAst32 = L[3 * r+1 : 4 * r, r-1 : 3 * r+1]
-
-
-#         # print(L32.shape, np.linalg.pinv(L22).shape, Wp.shape)
-#         Gamma_r = L32 @ np.linalg.pinv(L22) @ Wp  # oblique projection
-#         GammaAst_r = LAst32 @ np.linalg.pinv(LAst22) @ WpPlus  # oblique projection
-#         U_, s_, _ = scipy.linalg.svd(Gamma_r, full_matrices=False)
-
-#         self.singular_values = s_
-
-#         U1 = U_[:,0:n]
-#         Sigma_sqrt = np.diag(np.sqrt(s_[:n]))
-
-#         Or = U1 @ Sigma_sqrt
-#         Xf = np.linalg.pinv(Or) @ Gamma_r
-#         XfPlus =  np.linalg.pinv(Or[:-1,:]) @ GammaAst_r
-
-#         Y_ = np.vstack((XfPlus[:, : s - 2], self.y[r : r + s - 2].T))
-#         X_ = np.vstack((Xf[:, : s - 2], self.u[r : r + s - 2].T))
-#         Theta = Y_ @ np.linalg.pinv(X_)
-#         A = Theta[:n, :n]
-#         B = Theta[:n, n].ravel()
-#         C = Theta[n, :n].ravel()
-#         D = Theta[n, n]
-
-#         mod = StateSpaceModel(A=A, B=B, C=C, D=D, Ts=self.Ts)
-#         mod.info["Hankel singular values"] = s_
-
-#         return mod
-
-#     @staticmethod
-#     def name():
-#         return "n4sid2"
 
 
 class PO_MOESP(SubspaceIdent):
@@ -316,14 +180,10 @@ class PO_MOESP(SubspaceIdent):
         else:
             n = order
 
-        # N = self.y.shape[0]
-
         r = (2 * n + 1) * self.nu * self.ny  # window length
 
         Y = self.hankel(self.y, 2 * r)
         U = self.hankel(self.u, 2 * r)
-
-        # s = Y.shape[1]
 
         Yp = Y[0:r, :]
         Up = U[0:r, :]
@@ -332,22 +192,16 @@ class PO_MOESP(SubspaceIdent):
         Uf = U[r : 2 * r, :]
 
         Wp = np.vstack((Up, Yp))
-        # Wf = np.vstack((Uf,Yf))
-        # Psi = 1.0 / N * np.vstack((np.vstack((Uf, Wp)), Yf))
         Psi = np.vstack((np.vstack((Uf, Wp)), Yf))
 
         L, Q = self.lq(Psi)
 
         L11 = L[0:r, 0:r]
-        # L12 = L[  0:r  ,r:3*r]
-        # L21 = L[  r:3*r,0:r]
-        # L22 = L[  r:3*r,r:3*r]
         L31 = L[3 * r : 4 * r, 0:r]
         L32 = L[3 * r : 4 * r, r : 3 * r]
 
-        # Gamma_r = 1./np.sqrt(N) * L32
         Gamma_r = L32
-        # print(Gamma_r.shape)
+
         U_, s_, V_ = scipy.linalg.svd(Gamma_r, full_matrices=False)
 
         self.singular_values = s_
@@ -357,48 +211,8 @@ class PO_MOESP(SubspaceIdent):
         U1 = U_[:, 0:n]
         U2 = U_[:, n:r]
         Sigma_sqrt = np.diag(np.sqrt(s_[:n]))
-        # V1 = V_[:n, :]
-        # V1 = V_[:,0:n]
-        # V2 = V_[:,n:r]
 
-        # Or = U1 @ Sigma_sqrt  # extended observability matrix
-        # Xf = np.linalg.pinv(Or) @ Gamma_r
-        # print(Xf.shape)
-
-        # C = Or[0, :]# TODO: might be wrong!!!
-        # A = scipy.linalg.pinv(Or[0:-1, :]) @ Or[1:, :]
-        # # A = scipy.linalg.lstsq(Or[0:-1,:],Or[1:,:])
-        # # print(A)
-
-        # P = U2.T
-        # # print(P)
-        # A1_ = P.ravel(order="F")
-
-        # nn = A1_.shape[0]
-        # ny = 1
-        # A_ = np.zeros((nn, ny + n))
-        # A_[:, 0:ny] = A1_.reshape(-1, ny)
-
-        # for i in range(1, r):
-        #     Pi = P[:, i:r]
-        #     Oi = Or[0 : r - i, :]
-        #     Ni = Pi @ Oi
-        #     j = (i - 1) * (r - n)
-        #     A_[j : j + Ni.shape[0], ny : ny + Ni.shape[1]] = Ni
-
-        # # print(A_)
-
-        # M = (U2.T @ L31 @ np.linalg.inv(L11)).ravel(order="F")
-
-        # x_, *_ = scipy.linalg.lstsq(A_, M)
-        # # x_ = scipy.linalg.pinv(A_) @ M
-
-        # D = x_[0:ny]
-        # B = x_[ny : ny + n]
-
-        # print(s,n,r)
         A, B, C, D = self._abcd_observability_matrix(U1, U2, L11, L31, Sigma_sqrt, n, r)
-        # A, B, C, D = self._abcd_state(Xf, s, n, r)
 
         mod = StateSpaceModel(A=A, B=B, C=C, D=D, Ts=self.Ts)
         mod.info["Hankel singular values"] = s_
--- tests/test_subspace.py
+++ tests/test_subspace.py
@@ -48,120 +48,106 @@ def test_po_moesp(data_siso_deterministic, ss_mod):
 
 
 def test_n4sid_mimo(data_mimo_deterministic):
-    # data_mimo_deterministic.equidistant()
     identifyer = N4SID(data_mimo_deterministic, ["y0", "y1"], ["u0", "u1"])
     sys0 = N4SID(data_mimo_deterministic, "y0", "u0").ident(1)
     sys1 = N4SID(data_mimo_deterministic, "y1", "u1").ident(1)
     mod = identifyer.ident(2)
     print(mod.info["Hankel singular values"])
-    # np.testing.assert_allclose(
-    #     mod.info["Hankel singular values"],
-    #     [
-    #         6.40829670e00,
-    #         3.94039974e-04,
-    #         2.47748028e-08,
-    #         1.01130860e-08,
-    #         9.63664129e-09,
-    #         7.82223423e-09,
-    #         7.34439653e-09,
-    #         7.30785370e-09,
-    #         6.41191499e-09,
-    #         5.96869347e-09,
-    #         5.92269632e-09,
-    #         5.38759994e-09,
-    #         5.29767778e-09,
-    #         5.02836434e-09,
-    #         4.37323782e-09,
-    #         4.03590185e-09,
-    #         3.79029097e-09,
-    #         3.32988710e-09,
-    #         2.74121403e-09,
-    #         2.40889178e-09,
-    #     ],
-    #     rtol=1e-6,
-    #     atol=1e-6,
-    # )
+    np.testing.assert_allclose(
+        mod.info["Hankel singular values"],
+        [
+            5.23238711e01,
+            2.46007773e01,
+            9.15587701e-02,
+            9.90325106e-09,
+            9.65156298e-09,
+            8.85547497e-09,
+            8.51780379e-09,
+            8.32136576e-09,
+            7.44491698e-09,
+            7.03837731e-09,
+            5.99893485e-09,
+            5.85910395e-09,
+            5.52005230e-09,
+            5.26083503e-09,
+            5.10508405e-09,
+            4.67083640e-09,
+            3.59163096e-09,
+            3.53484111e-09,
+            3.01876150e-09,
+            2.64576961e-09,
+        ],
+        rtol=1e-6,
+        atol=1e-6,
+    )
 
-    # assert mod.A.shape == (2, 2)
-    # assert mod.B.shape == (2, 2)
-    # assert mod.C.shape == (2, 2)
-    # assert mod.D.shape == (2, 2)
+    assert mod.A.shape == (2, 2)
+    assert mod.B.shape == (2, 2)
+    assert mod.C.shape == (2, 2)
+    assert mod.D.shape == (2, 2)
 
     print(mod)
 
-    # np.testing.assert_allclose(
-    #    mod.to_controllable_form().A, ss_mod.A, rtol=1e-1, atol=1e-1
-    # )
-
-    print("step response sys0:\n", sys0.step_response(10)[1])
-    print("step response sys1:\n", sys1.step_response(10)[1])
     t, i = mod.step_response(10)
-    # assert i.shape == (10, 2)
-    print("step response mod :\n", i)
-    np.testing.assert_allclose(i, [[0], [0]])
-
-    # d = data_mimo_deterministic
-    # # y = mod.simulate(np.array([d["u0"], d["u1"]]).T)
-    # y = mod.simulate([d["u0"], d["u1"]])
-    # y_val = np.array([d["y0"], d["y1"]]).T
-    # np.testing.assert_allclose(y[1000:1010, :], y_val[1000:1010, :])
+    assert i.shape == (10, 2)
+    np.testing.assert_allclose(
+        i,
+        np.array(
+            [sys0.step_response(10)[1].ravel(), sys1.step_response(10)[1].ravel()]
+        ).T,
+        rtol=1e-5,
+        atol=1e-5,
+    )
 
 
 def test_po_moesp_mimo(data_mimo_deterministic):
-    # data_mimo_deterministic.equidistant()
     identifyer = PO_MOESP(data_mimo_deterministic, ["y0", "y1"], ["u0", "u1"])
     sys0 = PO_MOESP(data_mimo_deterministic, "y0", "u0").ident(1)
     sys1 = PO_MOESP(data_mimo_deterministic, "y1", "u1").ident(1)
     mod = identifyer.ident(2)
     print(mod.info["Hankel singular values"])
-    # np.testing.assert_allclose(
-    #     mod.info["Hankel singular values"],
-    #     [
-    #         6.40829670e00,
-    #         3.94039974e-04,
-    #         2.47748028e-08,
-    #         1.01130860e-08,
-    #         9.63664129e-09,
-    #         7.82223423e-09,
-    #         7.34439653e-09,
-    #         7.30785370e-09,
-    #         6.41191499e-09,
-    #         5.96869347e-09,
-    #         5.92269632e-09,
-    #         5.38759994e-09,
-    #         5.29767778e-09,
-    #         5.02836434e-09,
-    #         4.37323782e-09,
-    #         4.03590185e-09,
-    #         3.79029097e-09,
-    #         3.32988710e-09,
-    #         2.74121403e-09,
-    #         2.40889178e-09,
-    #     ],
-    #     rtol=1e-6,
-    #     atol=1e-6,
-    # )
+    np.testing.assert_allclose(
+        mod.info["Hankel singular values"],
+        [
+            5.13375445e01,
+            2.45203750e01,
+            9.15569690e-02,
+            9.88027549e-09,
+            9.63367326e-09,
+            8.84678201e-09,
+            8.50474100e-09,
+            8.31302996e-09,
+            7.42880630e-09,
+            7.02472812e-09,
+            5.99054895e-09,
+            5.85392065e-09,
+            5.51099680e-09,
+            5.25372488e-09,
+            5.10043257e-09,
+            4.66624062e-09,
+            3.58670073e-09,
+            3.52980561e-09,
+            3.01474135e-09,
+            2.64218261e-09,
+        ],
+        rtol=1e-6,
+        atol=1e-6,
+    )
 
-    # assert mod.A.shape == (2, 2)
-    # assert mod.B.shape == (2, 2)
-    # assert mod.C.shape == (2, 2)
-    # assert mod.D.shape == (2, 2)
+    assert mod.A.shape == (2, 2)
+    assert mod.B.shape == (2, 2)
+    assert mod.C.shape == (2, 2)
+    assert mod.D.shape == (2, 2)
 
     print(mod)
 
-    # np.testing.assert_allclose(
-    #    mod.to_controllable_form().A, ss_mod.A, rtol=1e-1, atol=1e-1
-    # )
-
-    print("step response sys0:\n", sys0.step_response(10)[1])
-    print("step response sys1:\n", sys1.step_response(10)[1])
     t, i = mod.step_response(10)
-    # assert i.shape == (10, 2)
-    print("step response mod :\n", i)
-    np.testing.assert_allclose(i, [[0], [0]])
-
-    # d = data_mimo_deterministic
-    # # y = mod.simulate(np.array([d["u0"], d["u1"]]).T)
-    # y = mod.simulate([d["u0"], d["u1"]])
-    # y_val = np.array([d["y0"], d["y1"]]).T
-    # np.testing.assert_allclose(y[1000:1010, :], y_val[1000:1010, :])
+    assert i.shape == (10, 2)
+    np.testing.assert_allclose(
+        i,
+        np.array(
+            [sys0.step_response(10)[1].ravel(), sys1.step_response(10)[1].ravel()]
+        ).T,
+        rtol=1e-5,
+        atol=1e-5,
+    )
