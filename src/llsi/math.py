d23d86883e932172de7c94a7ddb0d031b9fecf29
arminwitte <110226001+arminwitte@users.noreply.github.com>
2024-09-08 22:13:28 +0200

    Create math.py with numba code

---
+++ src/llsi/math.py
@@ -0,0 +1,15 @@
+from numba import njit
+
+@njit
+def evaluate_state_space(A, B, C, D, u, x1):
+    N = u.shape[1]
+    y = np.empty((N, self.ny))
+    for i, u_ in enumerate(u.T):
+        u_ = u_.T
+        u_ = u_.reshape(self.nu, 1)
+        x = x1
+        with np.errstate(over="ignore", invalid="ignore"):
+            x1 = self.A @ x + self.B @ u_
+            y_ = self.C @ x + self.D @ u_
+
+        y[i, :] = y_.ravel()
