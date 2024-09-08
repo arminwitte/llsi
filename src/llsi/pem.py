010b8d1791da7105a61a155aec2eeea773497987
arminwitte <110226001+arminwitte@users.noreply.github.com>
2024-09-08 17:31:21 +0200

    Add files via upload

--- src/llsi/pem.py
+++ src/llsi/pem.py
@@ -42,11 +42,12 @@ class PEM(SysIdAlgBase):
 
         x0 = mod.vectorize()
         method = self.settings.get("minimizer", "nelder-mead")
-        res = scipy.optimize.minimize(
-            fun, x0, method=method, options={"maxiter": 100, "maxfev": 100}
-        )
-        # res = scipy.optimize.minimize(fun,x0,method='nelder-mead')
-        # res = scipy.optimize.minimize(fun,x0,method='BFGS')
+        # res = scipy.optimize.minimize(
+           # fun, x0, method=method, options={"maxiter": 200, "maxfev": 200}
+        # (
+        # res = scipy.optimize.basinhopping(fun, x0, niter=1, minimizer_kwargs={"method":"BFGS", "options":{"maxiter":20}}, disp=True)
+        res = scipy.optimize.minimize(fun,x0,method='nelder-mead')
+        res = scipy.optimize.minimize(fun,res.x,method='BFGS',options={"gtol":1e-3})
 
         mod.reshape(res.x)
 
