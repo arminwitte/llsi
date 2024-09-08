8331bda0129a4ab743bd1c00576e48b756d8f924
arminwitte <110226001+arminwitte@users.noreply.github.com>
2024-09-08 22:23:16 +0200

    Fixed copy paste errors

--- src/llsi/math.py
+++ src/llsi/math.py
@@ -1,16 +1,19 @@
 from numba import njit
+import numpy as np
 
 @njit
 def evaluate_state_space(A, B, C, D, u, x1):
     N = u.shape[1]
-    y = np.empty((N, self.ny))
+    ny = C.shape[0]
+    nu = B.shape[1]
+    y = np.empty(N, ny))
     for i, u_ in enumerate(u.T):
         u_ = u_.T
-        u_ = u_.reshape(self.nu, 1)
+        u_ = u_.reshape(nu, 1)
         x = x1
         with np.errstate(over="ignore", invalid="ignore"):
-            x1 = self.A @ x + self.B @ u_
-            y_ = self.C @ x + self.D @ u_
+            x1 = A @ x + B @ u_
+            y_ = C @ x + D @ u_
 
         y[i, :] = y_.ravel()
     return y
