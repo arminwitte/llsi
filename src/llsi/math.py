8808679f0232223d76dc61f597594b054b3071db
arminwitte <110226001+arminwitte@users.noreply.github.com>
2024-09-08 22:37:35 +0200

    Enable reshape

--- src/llsi/math.py
+++ src/llsi/math.py
@@ -8,12 +8,12 @@ def evaluate_state_space(A, B, C, D, u, x1):
     nu = B.shape[1]
     y = np.empty((N, ny))
     for i, u_ in enumerate(u.T):
-        u_ = u_.T
-        u_ = u_.reshape(nu, 1)
+        ui = np.ascontiguousarray(u_.T)
+        ui = ui.reshape(nu, 1)
         x = x1
         # with np.errstate(over="ignore", invalid="ignore"):
-        x1 = A @ x + B @ u_
-        y_ = C @ x + D @ u_
+        x1 = A @ x + B @ ui
+        y_ = C @ x + D @ ui
 
         y[i, :] = y_.ravel()
     return y
